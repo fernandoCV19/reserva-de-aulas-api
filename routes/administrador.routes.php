bc34092c6519710e15c801275c028e1fea0ca130
Alejandro20032001 <dilanantezama@gmail.com>
2022-04-19 21:36:48 -0400

    prueba

--- routes/administrador.routes.php
+++ routes/administrador.routes.php
@@ -3,7 +3,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Reserva;
 
 
-
-Route::post('/respuesta-solicitud, 'App/Http/controllers/AdministradorController@crearReserva');
+Route::post('/respuesta-solicitud/{idSolicitud}', 'App/Http/controllers/AdministradorController@create');
