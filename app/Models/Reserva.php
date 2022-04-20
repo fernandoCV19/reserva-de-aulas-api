c4d86d8d3ebd359357d12f1b80d1730ac9ffaf6f
Fernando Cuenca Vargas <fernandocuva@gmail.com>
2022-04-19 20:59:41 -0400

    Relacion one to one y many to one creadas

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Reserva extends Model
 {
     use HasFactory;
+
+    public function datos(){
+        return $this->hasOne(DatosReserva::class, 'id_datos_reserva');
+    }
 }
