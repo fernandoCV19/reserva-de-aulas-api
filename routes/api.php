f3f8e39908f03a43bd916b6d973836a7fa7524e0
pabloskick <darkour.m@gmail.com>
2022-04-20 14:30:57 -0400

    corrección enpoint post solicitud reserva

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource("solicitudReserva","SolicitudReservaController");
+Route::post("/solicitudReserva","App\Http\Controllers\SolicitudReservaController@store");
