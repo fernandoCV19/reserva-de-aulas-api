bb43fdb7a6483f8a0f79d91423eee0b88af8f3bf
Alejandro20032001 <dilanantezama@gmail.com>
2022-04-20 11:34:01 -0400

    modelo solicitud reserva

--- app/Models/SolicitudReserva.php
+++ app/Models/SolicitudReserva.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\EstadoSolicitudEnum;
 
 class SolicitudReserva extends Model
 {
     use HasFactory;
-
+    
+    protected $table = 'solicitud_reserva';
+    public $timestamps = false;
+    protected $fillable = ['id, estado, fecha_creacion, id_datos_reserva'];
+    
+    
     public function datos_reserva(){
         return $this->hasOne(DatosReserva::class);
     }
+    
 }
