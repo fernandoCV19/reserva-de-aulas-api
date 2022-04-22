1b856e037ae3df4573a2a4929075415ea259052c
Fernando Cuenca Vargas <fernandocuva@gmail.com>
2022-04-21 22:22:12 -0400

    correcion en archivos base

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 50);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 50);
             $table->rememberToken();
             $table->timestamps();
         });
