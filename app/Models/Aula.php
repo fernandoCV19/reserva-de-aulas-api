ee206ec98607c49aaba9b0db5ad59c542cbdd84d
Alejandro20032001 <dilanantezama@gmail.com>
2022-04-24 14:22:55 -0400

    agregado tabla y fillable

--- app/Models/Aula.php
+++ app/Models/Aula.php
@@ -8,8 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Aula extends Model
 {
     use HasFactory;
+    protected $table = 'aulas';
+    protected $fillable = ['id, nombre, ubicacion, capacidad, descripcion'];
+
 
     public function datos_reservas(){
-        return $this->belongsTo(DatosReserva::class);
+        return $this->belongsTo(DatosReserva::class, "aula_datos_reserva", "aula_id", "datos_reserva_id");
     }
 }
