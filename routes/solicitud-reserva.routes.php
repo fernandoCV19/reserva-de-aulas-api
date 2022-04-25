de9300370ee5ce37637ef954fcfaac84efa70caa
Alejandro20032001 <dilanantezama@gmail.com>
2022-04-24 21:38:22 -0400

    ruta para filtro por proximidad

--- routes/solicitud-reserva.routes.php
+++ routes/solicitud-reserva.routes.php
@@ -7,7 +7,8 @@ use App\Models\SolicitudReserva;
 use App\Http\Controllers\SolicitudReservaController;
 
 
-Route::get('/proximos', [SolicitudReservaController::class, "getPendientesPorAntiguedad"]);
+Route::get('/antiguedad', [SolicitudReservaController::class, "getPendientesPorAntiguedad"]);
+Route::get('/proximos', [SolicitudReservaController::class, "getPendientesPorProximidad"]);
 Route::put('/cambio-estado/{idSolicitud}', [SolicitudReservaController::class, "actualizarEstado"]);
 
 Route::get("/user", function () {
