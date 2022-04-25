c9a697c8a902edfb3aca865aacd1cf7c12f983b2
pabloskick <darkour.m@gmail.com>
2022-04-25 18:30:38 -0400

    ruta busqueda id solicitud

--- routes/solicitud-reserva.routes.php
+++ routes/solicitud-reserva.routes.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\SolicitudReservaController;
 Route::post('/', [SolicitudReservaController::class, "crearSolitud"]);
 Route::get('/antiguedad', [SolicitudReservaController::class, "getPendientesPorAntiguedad"]);
 Route::get('/proximos', [SolicitudReservaController::class, "getPendientesPorProximidad"]);
+Route::get('/busqueda/{idSolicitud}',[SolicitudReservaController::class, "getSolicitudPorId"]);
 Route::put('/cambio-estado/{idSolicitud}', [SolicitudReservaController::class, "actualizarEstado"]);
 
 Route::get("/user", function () {
