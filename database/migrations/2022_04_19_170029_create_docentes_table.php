5e251e4af98f465cd65629c06228f86bd13b4d0f
Fernando Cuenca Vargas <fernandocuva@gmail.com>
2022-04-26 18:05:45 -0400

    Correcion

--- database/migrations/2022_04_19_170029_create_docentes_table.php
+++ database/migrations/2022_04_19_170029_create_docentes_table.php
@@ -15,7 +15,7 @@ class CreateDocentesTable extends Migration
     {
         Schema::create('docentes', function (Blueprint $table) {
             $table->uuid('id');
-            $table->integer('cod_SIS');
+            $table->string('cod_SIS', 20);
             $table->string('nombre', 200);
             $table->string('celular', 30);
             $table->string('contrasenia', 30);
--- database/seeders/MateriasRealesSeeder.php
+++ database/seeders/MateriasRealesSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
-class DocenteGrupoMateriaRealesSeeder extends Seeder
+class MateriasRealesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
