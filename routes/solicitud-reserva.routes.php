e715aee6dd02058cc4ea12d97886e6a9bda23179
Alejandro20032001 <dilanantezama@gmail.com>
2022-04-25 20:18:58 -0400

    cambios

--- routes/solicitud-reserva.routes.php
+++ routes/solicitud-reserva.routes.php
@@ -10,10 +10,10 @@ use App\Http\Controllers\SolicitudReservaController;
 Route::post('/', [SolicitudReservaController::class, "crearSolitud"]);
 Route::get('/antiguedad', [SolicitudReservaController::class, "getPendientesPorAntiguedad"]);
 Route::get('/proximos', [SolicitudReservaController::class, "getPendientesPorProximidad"]);
-Route::get('/{idSolicitud}',[SolicitudReservaController::class, "getSolicitudPorId"]);
 Route::get('/pendientes', [SolicitudReservaController::class, "getPendientes"]);
 Route::get('/estado/{idSolicitud}', [SolicitudReservaController::class, "getEstado"]);
 Route::put('/cambio-estado/{idSolicitud}', [SolicitudReservaController::class, "actualizarEstado"]);
+Route::get('/{idSolicitud}',[SolicitudReservaController::class, "getSolicitudPorId"]);
 
 
 Route::get("/user", function () {
