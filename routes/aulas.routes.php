a64f9be218bd774d2c84e918273832164c541b85
Alejandro20032001 <dilanantezama@gmail.com>
2022-04-27 22:02:47 -0400

    rutas disponibles para aulas

---
+++ routes/aulas.routes.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Aula;
+use App\Http\Controllers\AulasController;
+
+
+Route::get('/disponibles', [AulasController::class, "getDisponibles"]);
+//Route::get('/{idAula}',[SolicitudReservaController::class, "getSolicitudPorId"]);
+Route::get('/pendientes',[AulasController::class, "getPendientes"]);
+
+Route::get("/user", function () {
+    $mensaje =['mensaje'=>"Hola mundo"];
+    return response()->json($mensaje, 200,[]);
+});
