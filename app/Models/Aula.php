e80b3b3702fea86a4a5239ba2c73c7e8e5ae4e7e
Alejandro20032001 <dilanantezama@gmail.com>
2022-04-28 21:36:19 -0400

    uuid añadido

--- app/Models/Aula.php
+++ app/Models/Aula.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 
 class Aula extends Model
 {
     use HasFactory;
     protected $table = 'aulas';
+    public $timestamps = false;
+    public $incrementing = false;
     protected $fillable = ['id, nombre, ubicacion, capacidad, descripcion'];
 
 
