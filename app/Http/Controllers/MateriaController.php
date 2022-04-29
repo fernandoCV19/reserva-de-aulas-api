9dd701e22c362c03f9581762c5cd16ad99d51343
Alejandro20032001 <dilanantezama@gmail.com>
2022-04-28 21:25:08 -0400

    get docentes y grupos de una materia

--- app/Http/Controllers/MateriaController.php
+++ app/Http/Controllers/MateriaController.php
@@ -3,82 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Materia;
+use Illuminate\Support\Facades\DB;
+
 
 class MateriaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function getDocentes($idMateria){
+        $docentes=DB::table('grupos')
+        ->join("docentes", "docentes.id", "=", "grupos.docente_id")
+        ->where("materia_id", request("idMateria"))
+        ->select(["nombres as nombresDocente", "apellidos as apellidosDocente", "nombre as grupo"])
+        ->get();
+        return $docentes;
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    public function getGrupos($idMateria){
+        return Materia::find($idMateria)->grupos;
     }
 }
