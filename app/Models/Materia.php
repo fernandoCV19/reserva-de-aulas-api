19ec30ab1cfe66635f885ef3a46c9d81916dbd1b
Alejandro20032001 <dilanantezama@gmail.com>
2022-04-28 20:40:42 -0400

    model solicitud materia

--- app/Models/Materia.php
+++ app/Models/Materia.php
@@ -8,8 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Materia extends Model
 {
     use HasFactory;
-
+    protected $table = 'materias';
+    public $timestamps = false;
+    public $incrementing = false;
+    protected $fillable = ['id, nombre, codigo'];
     public function grupos(){
         return $this->hasMany(Grupo::class);
+    }
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->id = Str::uuid()->toString();
+        });
     }
 }
