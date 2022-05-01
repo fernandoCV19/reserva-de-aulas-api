827065d91f67bf7f9447adbec1185677b16b0f71
pabloskick <darkour.m@gmail.com>
2022-05-01 13:50:39 -0400

    creacion basica controlador notif

--- app/Http/Controllers/NotificacionController.php
+++ app/Http/Controllers/NotificacionController.php
@@ -4,82 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Notificacion;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class NotificacionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function crearNotificacion(Request $request)
     {
-        //
-    }
+        $notificacion  = new Notificacion();
+        $notificacion  -> mensaje = $request -> mensajeNotificacion;
+        $notificacion  ->fecha_creacion = now();
+        $notificacion  -> id_docente = $request->docente_id ;
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        $notificacion ->save();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return $notificacion ;
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Notificacion  $notificacion
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Notificacion $notificacion)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Notificacion  $notificacion
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Notificacion $notificacion)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Notificacion  $notificacion
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Notificacion $notificacion)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Notificacion  $notificacion
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Notificacion $notificacion)
-    {
-        //
+    
+    public function getNotificacionPorId(Request $request){
+        $notificacion =  Notificacion::findOrFail($request->idNotificacion);
+        return response()->json($notificacion, 200,[]);
     }
 }
