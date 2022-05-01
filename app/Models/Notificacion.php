0756403ca7038460bd82042fe57aae8ec8502e19
pabloskick <darkour.m@gmail.com>
2022-05-01 13:43:24 -0400

    configuracion modelo notificacion

--- app/Models/Notificacion.php
+++ app/Models/Notificacion.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Notificacion extends Model
 {
     use HasFactory;
+
+    protected $table = 'notificaciones';
+    public $timestamps = false;
+    public $incrementing = false;
+    protected $fillable = ['id, mensaje, fecha_notificacion, id_docente'];
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->id = Str::uuid()->toString();
+        });
+    }
+
+    public function docente(){
+        return $this->belongsTo(Docente::class);
+    }
 }
