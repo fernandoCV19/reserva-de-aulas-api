ec46aea2e002b4f090f3e7cff3f7f04b632821f8
pabloskick <darkour.m@gmail.com>
2022-05-01 13:01:36 -0400

    creacion controlador notificaciones

---
+++ app/Http/Controllers/NotificacionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notificacion;
+use Illuminate\Http\Request;
+
+class NotificacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Notificacion  $notificacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Notificacion $notificacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Notificacion  $notificacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Notificacion $notificacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Notificacion  $notificacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Notificacion $notificacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Notificacion  $notificacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Notificacion $notificacion)
+    {
+        //
+    }
+}
