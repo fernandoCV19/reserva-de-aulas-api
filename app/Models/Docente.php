bc4866be609d2500b8156cc913a01b775cf80187
Alejandro20032001 <dilanantezama@gmail.com>
2022-05-01 20:34:05 -0400

    Docente jwt

--- app/Models/Docente.php
+++ app/Models/Docente.php
@@ -4,12 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubeject;
 
-class Docente extends Model
+class Docente extends Model implements JWTSubeject
 {
     use HasFactory;
 
     public function grupos(){
         return $this->hasMany(Grupo::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
