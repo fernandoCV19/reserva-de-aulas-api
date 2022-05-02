eea1fa350cb92b142a15622e040f7b346a53e0d9
Fernando Cuenca Vargas <fernandocuva@gmail.com>
2022-05-01 21:52:03 -0400

    Relacion creada

--- app/Models/Notifacion.php
+++ app/Models/Notifacion.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Notifacion extends Model
 {
     use HasFactory;
+
+    public function docente(){
+        return $this->belongsTo(Docente::class);
+    }
 }
