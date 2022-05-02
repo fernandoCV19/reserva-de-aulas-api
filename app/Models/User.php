986761c08d0bb351f80ddc09a2c6d697d530d3e5
Alejandro20032001 <dilanantezama@gmail.com>
2022-05-01 20:34:20 -0400

    user jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubeject;
+
+class User extends Authenticatable implements JWTSubeject
 {
     use HasFactory, Notifiable;
 
@@ -37,4 +39,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
