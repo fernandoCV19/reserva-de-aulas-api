dcc991f55c632b0d0d6da98dc45e76b8e56d8803
Fernando Cuenca Vargas <fernandocuva@gmail.com>
2022-05-01 21:59:00 -0400

    Correciones terminadas

--- app/Models/Notificacion.php
+++ app/Models/Notificacion.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Notifacion extends Model
+class Notificacion extends Model
 {
     use HasFactory;
 
--- database/migrations/2022_04_19_190000_create_notificacions_table.php
+++ database/migrations/2022_04_19_190000_create_notificacions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNotifacionsTable extends Migration
+class CreateNotificacionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateNotifacionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('notifacions', function (Blueprint $table) {
+        Schema::create('notificacions', function (Blueprint $table) {
             $table->uuid('id');
             $table->string('mensaje', 500);
             $table->timestamp('fecha');
@@ -33,6 +33,6 @@ class CreateNotifacionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notifacions');
+        Schema::dropIfExists('notificacions');
     }
 }
