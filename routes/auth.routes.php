4e0fca810208b208967bab51e62a8e3ff506ab29
Alejandro20032001 <dilanantezama@gmail.com>
2022-05-05 11:03:48 -0400

    aumento en las rutas

---
+++ routes/auth.routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::middleware('auth:api')->get('/docente', function (Request $request) {
+    return $request->docente();
+});
+Route::group([
+    'middleware' => 'api',
+
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('activar', [AuthController::class, 'activar']);
+});
+
