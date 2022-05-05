696b8d2cbfbb5a4255d78547244086fb446c1e94
Alejandro20032001 <dilanantezama@gmail.com>
2022-05-05 11:04:17 -0400

    correccion en las rutas permitidas

--- routes/docentes.routes.php
+++ routes/docentes.routes.php
@@ -1,19 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DocenteController;
 
-Route::group([
-
-'middleware' => 'api',
-'prefix' => 'auth'
-
-], function ($router) {
-
-Route::post('login', [AuthController::class,'login']);
-Route::post('logout', [AuthController::class, 'logout']);
-Route::post('refresh', [AuthController::class, 'refresh']);
-Route::post('me', [AuthController::class, 'me']);
-
-});
 Route::get('/notificaciones/{idDocente}', [DocenteController::class, 'getNotificacionesByDocente']);
