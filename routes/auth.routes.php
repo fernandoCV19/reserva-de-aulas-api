3e631b515346d32492b1071d1926397f26773285
Fernando Cuenca Vargas <fernandocuva@gmail.com>
2022-05-06 16:26:27 -0400

    Fix rapido de auth.routes

--- routes/auth.routes.php
+++ routes/auth.routes.php
@@ -4,14 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:api')->get('/docente', function (Request $request) {
-    return $request->docente();
-});
+Route::post('login', [AuthController::class, 'login']);
+Route::post('activar', [AuthController::class, 'activar']);
+
 Route::group(['middleware' => ['jwt.verify']], function() {
-    Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('me', [AuthController::class, 'me']);
-    Route::post('activar', [AuthController::class, 'activar']);
 });
 
