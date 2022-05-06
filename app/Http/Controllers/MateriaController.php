3b3bdccfd8b66ea91953a5adefafd190e18f39cc
Alejandro20032001 <dilanantezama@gmail.com>
2022-05-05 22:27:36 -0400

    Documentacion de materia

--- app/Http/Controllers/MateriaController.php
+++ app/Http/Controllers/MateriaController.php
@@ -9,6 +9,31 @@ use Illuminate\Support\Facades\DB;
 
 class MateriaController extends Controller
 {
+    /**
+     * @OA\Get(
+     *      path= "/materia/docentes/{idMateria}",
+     *      summary =  "Obtencion de una todos los docentes de una materia",
+     *      tags = {"Materia"},
+     * 
+     *      @OA\Parameter(
+     *          name="idMateria",
+     *          description="Id de la Materia",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),     
+     *      @OA\Response(
+     *          response=200,
+     *          description = "OK"),
+     *      @OA\Response(
+     *         response="default",
+     *         description="Ha ocurrido un error."
+     *      ) 
+     * )
+     * 
+     */
     public function getDocentes($idMateria){
         $docentes=DB::table('grupos')
         ->join("docentes", "docentes.id", "=", "grupos.docente_id")
@@ -19,6 +44,31 @@ class MateriaController extends Controller
         ->get();
         return $docentes;
     }
+    /**
+     * @OA\Get(
+     *      path= "/materia/grupos/{idMateria}",
+     *      summary =  "Obtencion de una todos los grupos apartir de un id de materia",
+     *      tags = {"Materia"},
+     * 
+     *      @OA\Parameter(
+     *          name="idMateria",
+     *          description="Id de la Materia",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),     
+     *      @OA\Response(
+     *          response=200,
+     *          description = "OK"),
+     *      @OA\Response(
+     *         response="default",
+     *         description="Ha ocurrido un error."
+     *      ) 
+     * )
+     * 
+     */
     public function getGrupos($idMateria){
         return Materia::find($idMateria)->grupos;
     }
