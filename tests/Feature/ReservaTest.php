e65a5251b86b2359e762006fb3c3927eae317094
Fernando Cuenca Vargas <fernandocuva@gmail.com>
2022-05-05 23:35:39 -0400

    Test inicio

--- tests/Feature/ReservaTest.php
+++ tests/Feature/ReservaTest.php
@@ -5,18 +5,39 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Database\Seeders\ReservaTestSeeder;
 
 class ReservaTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+    protected $seeder = ReservaTestSeeder::class;
+ 
+    public function test_solicitar_aulas_de_datos_de_reserva()
     {
-        $response = $this->get('/');
+        $response = $this->get('reserva/aulas/1');
 
         $response->assertStatus(200);
+    }
+
+    public function test_verificar_cantidad_aulas_de_datos_de_reserva()
+    {
+        $response = json_decode($this->get('reserva/aulas/1')->getContent(), true);
+
+        $tam = sizeof($response['aulas']);
+
+        $this -> assertEquals($tam, 3);
+    }
+
+    public function test_verificar_aulas_de_datos_de_reserva()
+    {
+        $response = json_decode($this->get('reserva/aulas/1')->getContent(), true);
+
+        print_r($response);
+
+        $verificacion1 = $response['aulas'][0]['nombre'] == "691F";
+        $verificacion2 = $response['aulas'][1]['nombre'] == "692F";
+        $verificacion3 = $response['aulas'][2]['nombre'] == "693F";
+
+        $this -> assertTrue($verificacion1 and $verificacion2 and $verificacion3);
     }
 }
--- tests/Feature/SolicitudReservasTest.php
+++ tests/Feature/SolicitudReservasTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class SolicitudReservaTest extends TestCase
+class SolicitudReservasTest extends TestCase
 {
     /**
      * A basic feature test example.
