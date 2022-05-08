20080e655e06d1e6302b3df9182c37cb64d315f4
Alejandro20032001 <dilanantezama@gmail.com>
2022-05-08 17:42:13 -0400

    borrado de espacio

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -18,13 +18,14 @@ class JwtMiddleware
     public function handle($request, Closure $next)
     {
         try {
+            
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
                 return response()->json(['status' => 'Token is Invalid'], 401);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
                 return response()->json(['status' => 'Token is Expired'], 401);
-            }else{
+            }else{                
                 return response()->json(['status' => 'Authorization Token not found'], 401);
             }
         }
