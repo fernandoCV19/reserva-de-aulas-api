8a7b01f9d8077446c7754ddce39202413eeac0eb
Alejandro20032001 <dilanantezama@gmail.com>
2022-05-08 17:40:03 -0400

    correccion llamada al modelo

--- app/Http/Controllers/AulasController.php
+++ app/Http/Controllers/AulasController.php
@@ -24,7 +24,7 @@ class AulasController extends Controller
      * 
      */
     public function getTodasAulas(){
-        return Aulas::all();
+        return Aula::all();
     }
     /**
      * @OA\Post(
