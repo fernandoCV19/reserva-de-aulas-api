12d97098dc0a079dc6ab9d302bbfe3e361b2f279
pabloskick <darkour.m@gmail.com>
2022-05-16 12:37:06 -0400

    nuevas rutas

--- routes/aulas.routes.php
+++ routes/aulas.routes.php
@@ -13,6 +13,8 @@ Route::post('/pendientes',[AulasController::class, "getPendientes"]);
 Route::post('/',[AulasController::class, "crearAula"]);
 Route::post('/areas',[AulasController::class, "filtrarAulasPorAreas"]);
 Route::post('/cantidad',[AulasController::class, "filtrarAulasPorCantidad"]);
+Route::post('/periodo',[AulasController::class, "filtrarAulasPorPeriodo"]);
+Route::post('/estado-aula',[AulasController::class, "aulaEstado"]);
 Route::post('/general',[AulasController::class, "filtrarGeneral"]);
 Route::get("/user", function () {
     $mensaje =['mensaje'=>"Hola mundo"];
