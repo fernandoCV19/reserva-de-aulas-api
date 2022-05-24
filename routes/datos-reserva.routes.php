ef4a4169108c81b916e85f1be5577ad8eac0d739
Alejandro20032001 <dilanantezama@gmail.com>
2022-05-23 22:46:09 -0400

    ruta materia

--- routes/datos-reserva.routes.php
+++ routes/datos-reserva.routes.php
@@ -15,6 +15,7 @@ Route::get("/user", function () {
 Route::post('/', [DatosReservaController::class, "crearDatosReserva"]);
 Route::get('/aulas/{idDatosReserva}', [DatosReservaController::class, "getAulasDatosReserva"]);
 Route::delete('/aulas/{idDatosReserva}', [DatosReservaController::class, "deleteAulas"]);
+Route::get('/materia/{idDatosReserva}', [DatosReservaController::class, "getMateria"]);
 Route::get('/{idDatosReserva}', [DatosReservaController::class, "getDatosReservaById"]);
 
 
